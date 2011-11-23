4711fc2b4894aa8dec79edfb495516e32a2e753d
jonhack <jonathanh@iqmetrix.com>
2011-11-23 12:17:18 -0600

    Pushing new changes

---
+++ source/app.specs/BasicRequestProcessorSpecs.cs
@@ -0,0 +1,34 @@
+﻿using Machine.Specifications;
+using app.web.core;
+using developwithpassion.specifications.rhinomocks;
+
+namespace app.specs
+{
+    [Subject(typeof(BasicRequestProcessor))]
+    public class BasicRequestProcessorSpecs
+    {
+        public abstract class concern : Observes<IProcessOneRequest,
+                                            BasicRequestProcessor>
+        {
+        }
+
+        public class when_processing_a_command : concern
+        {
+            Establish c = () =>
+            {
+                request = fake.an<IContainRequestInformation>();
+            };
+
+            Because b = () =>
+                result = sut.can_handle(request);
+
+            It should_handle_a_request = () =>
+            {
+                result.ShouldEqual(true);
+            };
+
+            static IContainRequestInformation request;
+            static bool result;
+        }
+    }
+}
---
+++ source/app/web/core/BasicRequestProcessor.cs
@@ -0,0 +1,16 @@
+﻿namespace app.web.core
+{
+    public class BasicRequestProcessor : IProcessOneRequest
+    {
+
+        public void process(IContainRequestInformation request)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool can_handle(IContainRequestInformation request)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
